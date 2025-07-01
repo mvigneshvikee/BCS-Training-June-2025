dd5aea74f9124d50f9e08f5ab146278e330d501d
mvigneshvikee <vigneshvikee0001@gmail.com>
2025-07-01 11:19:58 +0530

    Array_Methods Assignment

--- Day_09/Array09_Methods.js
+++ Day_09/Array09_Methods.js
@@ -12,7 +12,7 @@
 // const reverseNUm = num.reverse()  // reverse returns objects
 
 // console.log(num)
-// console.log(reverseNUm)     // copy by reference 
+// console.log(reverseNUm)     // copy by reference
 // console.log(typeof reverseNUm)
 
 // const y1 ="present"
@@ -25,8 +25,6 @@
 // }else{
 //     console.log("Class Not Started")
 // }
-
-
 
 // const students = [
 //     {name:"Aarav", status:"present"},
@@ -47,54 +45,53 @@
 //     console.log("Class Not Started")
 // }
 
+// =========================================================================
 
-// 
+const movies = [
+  {
+    id: 1,
+    title: "Baahubali",
+    director: "S. S. Rajamouli",
+    year: 2015,
+    ratings: [8, 9, 10],
+    genre: "Action",
+  },
+  {
+    id: 2,
+    title: "Arjun Reddy",
+    director: "Sandeep Reddy Vanga",
+    year: 2017,
+    ratings: [9, 8, 9],
+    genre: "Drama",
+  },
+  {
+    id: 3,
+    title: "Mahanati",
+    director: "Nag Ashwin",
+    year: 2018,
+    ratings: [10, 9, 6],
+    genre: "Biography",
+  },
+  {
+    id: 4,
+    title: "Eega",
+    director: "S. S. Rajamouli",
+    year: 2012,
+    ratings: [7, 8, 9],
+    genre: "Fantasy",
+  },
+  {
+    id: 5,
+    title: "Jersey",
+    director: "Gowtam Tinnanuri",
+    year: 2019,
+    ratings: [9, 9, 8],
+    genre: "Sports",
+  },
+];
 
-// const movies = [
-//   {
-//     id: 1,
-//     title: "Baahubali",
-//     director: "S. S. Rajamouli",
-//     year: 2015,
-//     ratings: [8, 9, 10],
-//     genre: "Action",
-//   },
-//   {
-//     id: 2,
-//     title: "Arjun Reddy",
-//     director: "Sandeep Reddy Vanga",
-//     year: 2017,
-//     ratings: [9, 8, 9],
-//     genre: "Drama",
-//   },
-//   {
-//     id: 3,
-//     title: "Mahanati",
-//     director: "Nag Ashwin",
-//     year: 2018,
-//     ratings: [10, 9, 8],
-//     genre: "Biography",
-//   },
-//   {
-//     id: 4,
-//     title: "Eega",
-//     director: "S. S. Rajamouli",
-//     year: 2012,
-//     ratings: [7, 8, 9],
-//     genre: "Fantasy",
-//   },
-//   {
-//     id: 5,
-//     title: "Jersey",
-//     director: "Gowtam Tinnanuri",
-//     year: 2019,
-//     ratings: [9, 9, 8],
-//     genre: "Sports",
-//   },
-// ];
-
-// // ===== YOUR CODE BELOW =====
-// // Write a function that finds a movie by ID and returns formatted details
+// ===== YOUR CODE BELOW =====
+// Write a function that finds a movie by ID and returns formatted details
 // const getMovieDetails = (movies, id) => {
 //   // Your code here
 //   const filterMovie = movies.filter(movie => movie.id==id)
@@ -102,27 +99,56 @@
 //     return `Movie Not Found`
 //   }
 //   return `${filterMovie[0].title} is a ${filterMovie[0].genre} movie`
-  
+
 // };
+
+const allRatingsAboveForGenre = (movies, rating, genre) => {
+  const genreMovies = movies.filter((movie) => movie.genre == genre);
+  const movieRating = genreMovies.every((movie) =>
+    movie.ratings.every((r) => r > rating));
+  console.log(movieRating)
+  if(movieRating) {
+    return `Yes, all ${genre} movies are above ${rating} ratings`;
+  } else {
+    return `No, not all ${genre} movies are above ${rating} ratings`;
+  }
+};
+
+console.log(allRatingsAboveForGenre(movies, 7, "Action"));
+console.log(allRatingsAboveForGenre(movies, 8, "Biography"));
+
+// Yes, all Action movies are above 7 ratings
+// No, not all Biography movies are above 8 ratings
 
 // console.log(getMovieDetails(movies, 1));
 // console.log(getMovieDetails(movies, 5));
 // console.log(getMovieDetails(movies, 6));
 
-const books = [
-  { title: "Infinite Jest", rating: 4.5, genre: "Fiction" },
-  { title: "The Catcher in the Rye", rating: 3.9, genre: "Fiction" },
-  { title: "Sapiens", rating: 4.9, genre: "History" },
-  { title: "A Brief History of Time", rating: 4.8, genre: "Science" },
-  { title: "Clean Code", rating: 4.7, genre: "Technology" },
-];
-const getTitles1 = (books) => books.map(book => book.title)
-const getTitles = (books) => {
-  const movieFiction = books.filter(book => book.genre == "Fiction")
-// console.log(movieGenre)
-const fictionMovies = movieFiction.map(movie => movie.title)
-return fictionMovies
-}
+//======================================================================================
+// const books = [
+//   { title: "Infinite Jest", rating: 4.5, genre: "Fiction" },
+//   { title: "The Catcher in the Rye", rating: 3.9, genre: "Fiction" },
+//   { title: "Sapiens", rating: 4.9, genre: "History" },
+//   { title: "A Brief History of Time", rating: 4.8, genre: "Science" },
+//   { title: "Clean Code", rating: 4.7, genre: "Technology" },
+// ];
+// const getTitles1 = (books) => books.map(book => book.title)
+// const getTitles = (books) => {
+//   const movieFiction = books.filter(book => book.genre == "Fiction")
+// // console.log(movieGenre)
+// const fictionMovies = movieFiction.map(movie => movie.title)
+// return fictionMovies
+// }
 
-console.log(getTitles(books))
-console.log(getTitles1(books))
+// console.log(getTitles(books))
+// console.log(getTitles1(books))
+
+// const person = { name: "Tom",
+//   age: 28 ,
+//   Adre: {
+//     city : "kk",
+//     vil :"ll"
+//   }
+// };
+// const { name, age,Adre:{city,vill} } = person;
+// console.log(name, age);
--- Website_Assignment/Arraay_Methods.md
+++ Website_Assignment/Arraay_Methods.md
@@ -175,16 +175,64 @@ Write a function that checks if all movies of a certain genre have ratings above
 ### Answer
 
 ```js
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const movies = [
+  {
+    id: 1,
+    title: "Baahubali",
+    director: "S. S. Rajamouli",
+    year: 2015,
+    ratings: [8, 9, 10],
+    genre: "Action",
+  },
+  {
+    id: 2,
+    title: "Arjun Reddy",
+    director: "Sandeep Reddy Vanga",
+    year: 2017,
+    ratings: [9, 8, 9],
+    genre: "Drama",
+  },
+  {
+    id: 3,
+    title: "Mahanati",
+    director: "Nag Ashwin",
+    year: 2018,
+    ratings: [10, 9, 6],
+    genre: "Biography",
+  },
+  {
+    id: 4,
+    title: "Eega",
+    director: "S. S. Rajamouli",
+    year: 2012,
+    ratings: [7, 8, 9],
+    genre: "Fantasy",
+  },
+  {
+    id: 5,
+    title: "Jersey",
+    director: "Gowtam Tinnanuri",
+    year: 2019,
+    ratings: [9, 9, 8],
+    genre: "Sports",
+  },
+];
 
-// ===== YOUR CODE BELOW =====
-// Use filter to keep only even numbers
-const getEvenNumbers = (numbers) => {
-  // Your code here
-  return numbers.filter(n => n%2 == 0)
+const allRatingsAboveForGenre = (movies, rating, genre) => {
+  const genreMovies = movies.filter((movie) => movie.genre == genre);
+  const movieRating = genreMovies.every((movie) =>
+    movie.ratings.every((r) => r > rating));
+  console.log(movieRating)
+  if(movieRating) {
+    return `Yes, all ${genre} movies are above ${rating} ratings`;
+  } else {
+    return `No, not all ${genre} movies are above ${rating} ratings`;
+  }
 };
 
-console.log(JSON.stringify(getEvenNumbers(numbers)));
+console.log(allRatingsAboveForGenre(movies, 7, "Action"));
+console.log(allRatingsAboveForGenre(movies, 8, "Biography"));
+
 
 ```
 ### Expected Output:
